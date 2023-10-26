84abc1b7aa157de2ba6a9973be629645df2f4a75
Kitcht <oteamton@gmail.com>
2023-10-26 16:39:17 +0700

    Import neccessary data for test.

--- db/ent_db.sql
+++ db/ent_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 22, 2023 at 04:44 PM
+-- Generation Time: Oct 26, 2023 at 11:35 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -24,19 +24,73 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `individual_roles_types`
+--
+
+CREATE TABLE `individual_roles_types` (
+  `id` int(11) NOT NULL,
+  `role_type` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `organization_roles_types`
+--
+
+CREATE TABLE `organization_roles_types` (
+  `id` int(11) NOT NULL,
+  `role_type` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sessions`
+--
+
+CREATE TABLE `sessions` (
+  `id` int(11) NOT NULL,
+  `session_id` varchar(255) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `sessions`
+--
+
+INSERT INTO `sessions` (`id`, `session_id`, `user_id`, `created_at`) VALUES
+(1, '608fcab61c0e6018eddcc6910c9b43588be8b734b4dc81df62fd544a49a4f92f', 3, '2023-10-24 06:06:13'),
+(2, 'c06df8df8c2e86eeecb1bde63aab58ecf3945522f320b031170d59feebb3c6be', 4, '2023-10-24 09:01:34'),
+(3, '7107caea5d95cc1607346c34fca4c97369831db5aa119163a03d58f23cf5c4d2', 9, '2023-10-26 09:11:31');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `temp_users`
 --
 
 CREATE TABLE `temp_users` (
-  `id` int(11) NOT NULL,
+  `temp_id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `surname` varchar(255) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `surname` varchar(255) DEFAULT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `token` varchar(255) DEFAULT NULL,
-  `token_expiration` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `token` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `expiration` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Triggers `temp_users`
+--
+DELIMITER $$
+CREATE TRIGGER `set_expiration_timestamp` BEFORE INSERT ON `temp_users` FOR EACH ROW BEGIN
+    SET NEW.expiration = NOW() + INTERVAL 1 HOUR;
+END
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -46,22 +100,94 @@ CREATE TABLE `temp_users` (
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `surname` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `username` varchar(30) NOT NULL,
+  `NAME` varchar(30) DEFAULT NULL,
+  `surname` varchar(30) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `PASSWORD` varchar(255) NOT NULL,
+  `role_id` int(11) DEFAULT 1,
+  `role_status_id` int(11) DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `NAME`, `surname`, `email`, `PASSWORD`, `role_id`, `role_status_id`) VALUES
+(3, 'kitchanunt', '', '', 'oteamton@gmail.com', '$2y$10$7LfdR7GvkQFlQYyvb8g1hekbcz0EjySFkpOUUbIP44hKz02jb0AZy', 1, 3),
+(4, 'ent_admin', 'Admin', 'Engagement', 'ent_th@gmail.com', '$2y$10$.rdUSWqtX8Kmpq9aQ7mEOuZMV.xTWy.cqjWguSIQ48KKo7jtGjbwK', 4, 2),
+(9, 'tonton', 't', 't', 'zelazideqc@gmail.com', '$2y$10$nkk6qp6tOORNlLlNSFyzaO77c3KHdwOr999t6gmrX03MtkHLA/6uG', 1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_roles`
+--
+
+CREATE TABLE `users_roles` (
+  `id` int(11) NOT NULL,
+  `role_name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users_roles`
+--
+
+INSERT INTO `users_roles` (`id`, `role_name`) VALUES
+(1, 'guest'),
+(2, 'Organization'),
+(3, 'Individual'),
+(4, 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_roles_status`
+--
+
+CREATE TABLE `users_roles_status` (
+  `id` int(11) NOT NULL,
+  `status_name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users_roles_status`
+--
+
+INSERT INTO `users_roles_status` (`id`, `status_name`) VALUES
+(1, 'pending'),
+(2, 'active'),
+(3, 'unactive');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `individual_roles_types`
+--
+ALTER TABLE `individual_roles_types`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `organization_roles_types`
+--
+ALTER TABLE `organization_roles_types`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sessions`
+--
+ALTER TABLE `sessions`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `session_id` (`session_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
 -- Indexes for table `temp_users`
 --
 ALTER TABLE `temp_users`
-  ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`temp_id`),
   ADD UNIQUE KEY `username` (`username`),
   ADD UNIQUE KEY `email` (`email`);
 
@@ -71,23 +197,79 @@ ALTER TABLE `temp_users`
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`),
-  ADD UNIQUE KEY `email` (`email`);
+  ADD UNIQUE KEY `NAME` (`NAME`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `role_id` (`role_id`),
+  ADD KEY `role_status_id` (`role_status_id`);
+
+--
+-- Indexes for table `users_roles`
+--
+ALTER TABLE `users_roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users_roles_status`
+--
+ALTER TABLE `users_roles_status`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `organization_roles_types`
+--
+ALTER TABLE `organization_roles_types`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sessions`
+--
+ALTER TABLE `sessions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `temp_users`
 --
 ALTER TABLE `temp_users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `temp_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `users_roles`
+--
+ALTER TABLE `users_roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `users_roles_status`
+--
+ALTER TABLE `users_roles_status`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `sessions`
+--
+ALTER TABLE `sessions`
+  ADD CONSTRAINT `sessions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `role_id` FOREIGN KEY (`role_id`) REFERENCES `users_roles` (`id`),
+  ADD CONSTRAINT `role_status_id` FOREIGN KEY (`role_status_id`) REFERENCES `users_roles_status` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
