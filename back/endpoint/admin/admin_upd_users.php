68b30fe44ca3c453ca7a61bb1ba57f777d4b07a0
Kitcht <oteamton@gmail.com>
2023-11-01 14:02:16 +0700

    Added send email to user that has been update role

--- back/endpoint/admin/admin_upd_users.php
+++ back/endpoint/admin/admin_upd_users.php
@@ -1,21 +1,29 @@
 <?php
 require(dirname(__DIR__) . '/../database/connection.php');
+require(dirname(__DIR__) . '/../utils/email.php');
 
 header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: http://localhost:3000'); 
+header('Access-Control-Allow-Origin: http://localhost:3000');
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 $input = json_decode(file_get_contents('php://input'), true);
 
-if(isset($input['user_id']) && isset($input['role_status_id'])) {
+if (isset($input['user_id']) && isset($input['role_status_id'])) {
   $userId = $input['user_id'];
   $newStatusRoleId = $input['role_status_id'];
+  $email = $input['email'];
+  $subject = "Thanks for joining us! Your transcation has been checked";
+  $messageBody = "Your account has been updated to Active";
 
   $stmt = $mysqli->prepare("UPDATE users SET role_status_id = ? WHERE id = ?");
   $stmt->bind_param("ii", $newStatusRoleId, $userId);
 
-  if($stmt->execute()) {
+  if (isset($input['role_status_id']) == 2) {
+    (sendEmail($email, $subject, $messageBody));
+  }
+
+  if ($stmt->execute()) {
     echo json_encode(['status' => 'success']);
   } else {
     echo json_encode(['status' => 'error', 'message' => 'Could not update role']);
